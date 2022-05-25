e04f5ebd99d773496b6a279d047f3b267b1e43f6
KELLY BIRG <93093928+kellybirg@users.noreply.github.com>
2022-05-24 17:09:57 -0700

    H19 - 5/24 - Final Attempt
    Added Button Hover states, and additional links!

---
+++ js/index.js
@@ -0,0 +1,10 @@
+console.log("Javascript File Connected Successfully");
+
+
+function down1() {
+    document.getElementById("portfolio").scrollIntoView();
+}
+
+function down2() {
+    document.getElementById("connect").scrollIntoView();
+}
